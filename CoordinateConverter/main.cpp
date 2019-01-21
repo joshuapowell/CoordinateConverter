6eeb5fd3952da641ef44aa5dc93f387cc7e908be
Joshua Powell <joshua@viable.io>
2019-01-20 20:00:19 -0500

    Initial Commit

---
+++ CoordinateConverter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CoordinateConverter
+//
+//  Created by Joshua Powell on 1/20/19.
+//  Copyright © 2019 Joshua Powell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
